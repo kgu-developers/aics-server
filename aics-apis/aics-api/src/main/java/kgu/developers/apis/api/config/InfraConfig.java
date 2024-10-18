a657cba60168fd1eacdbdba9cbf9ff747a2e71b7
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-10-18 14:09:28 +0900

    refactor: gitkeep 파일 추가 및 InfraConfig config 하위로 이동

--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/config/InfraConfig.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/config/InfraConfig.java
@@ -1,4 +1,4 @@
-package kgu.developers.apis.api;
+package kgu.developers.apis.api.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
