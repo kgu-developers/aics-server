403698a37a1f0bc887ab19f77f1e27ca92c31202
minjo-on <alswns11346@gmail.com>
2024-10-18 19:08:30 +0900

    fix: majorid 주석 해제, 메소드에 major 관련 내용 추가

--- aics-core/aics-domain/src/main/java/kgu/developers/core/domain/user/User.java
+++ aics-core/aics-domain/src/main/java/kgu/developers/core/domain/user/User.java
@@ -2,6 +2,7 @@ package kgu.developers.core.domain.user;
 
 import jakarta.persistence.*;
 import kgu.developers.core.common.domain.BaseTimeEntity;
+import kgu.developers.core.domain.major.Major;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,61 +17,61 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 public class User extends BaseTimeEntity {
-    @Id
-    @Column(name = "user_id")
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
 
-    @Column(unique = true, nullable = false, updatable = false, length = 10)
-    private String personalId;
+	@Column(unique = true, nullable = false, updatable = false, length = 10)
+	private String personalId;
 
-    @Column(nullable = false, length = 20)
-    private String password;
+	@Column(nullable = false, length = 20)
+	private String password;
 
-    @Column(nullable = false, length = 20)
-    private String name;
+	@Column(nullable = false, length = 20)
+	private String name;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
 
-    @Column(nullable = false, length = 8)
-    private String birth;
+	@Column(nullable = false, length = 8)
+	private String birth;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Grade grade;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Grade grade;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Status status;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Status status;
 
-    @Column(name = "user_role", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Role role;
+	@Column(name = "user_role", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
-    @Column(nullable = false)
-    private boolean hasAiAccess;
+	@Column(nullable = false)
+	private boolean hasAiAccess;
 
-    /*
-    // 전공 테이블 연결
-    @Column(nullable = false)
-    @ManyToOne(fetch = LAZY)
-    private Major majorId;
-     */
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(nullable = false)
+	private Major majorId;
 
-    private User(String personalId, String password, String name, Gender gender, String birth, Grade grade, Status status, Role role) {
-        this.personalId = personalId;
-        this.password = password;
-        this.name = name;
-        this.gender = gender;
-        this.birth = birth;
-        this.status = status;
-        this.grade = grade;
-        this.role = role;
-    }
+	private User(String personalId, String password, String name, Gender gender, String birth, Grade grade,
+		Status status, Role role, Major majorId) {
+		this.personalId = personalId;
+		this.password = password;
+		this.name = name;
+		this.gender = gender;
+		this.birth = birth;
+		this.status = status;
+		this.grade = grade;
+		this.role = role;
+		this.majorId = majorId;
+	}
 
-    public static User of(String personalId, String password, String name, Gender gender, String birth, Grade grade, Status status, Role role){
-        return new User(personalId, password, name, gender, birth, grade, status, role);
-    }
+	public static User of(String personalId, String password, String name, Gender gender, String birth, Grade grade,
+		Status status, Role role, Major majorId) {
+		return new User(personalId, password, name, gender, birth, grade, status, role, majorId);
+	}
 }
