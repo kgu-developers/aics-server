4b86925f586cd2974a6d17f973fd9a9c6368389a
minjo-on <alswns11346@gmail.com>
2024-10-20 05:24:09 +0900

    fix: BcryptPasswordEncoder 빈 등록 안됨 해결 #19

--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/user/application/UserService.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/user/application/UserService.java
@@ -1,11 +1,14 @@
 package kgu.developers.apis.api.user.application;
 
+import kgu.developers.core.common.config.SecurityConfig;
 import kgu.developers.core.domain.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Import(SecurityConfig.class)
 @RequiredArgsConstructor
 public class UserService {
 	private final UserRepository userRepository;
