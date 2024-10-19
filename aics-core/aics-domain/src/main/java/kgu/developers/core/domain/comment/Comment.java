ef56081578c347d62dcc9108d7458fbc491f86e3
minjo-on <alswns11346@gmail.com>
2024-10-19 16:38:04 +0900

    refactor: create 메소드에 post 추가

--- aics-core/aics-domain/src/main/java/kgu/developers/core/domain/comment/Comment.java
+++ aics-core/aics-domain/src/main/java/kgu/developers/core/domain/comment/Comment.java
@@ -1,9 +1,12 @@
 package kgu.developers.core.domain.comment;
 
+import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.GenerationType.*;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import kgu.developers.core.common.domain.BaseTimeEntity;
@@ -12,9 +15,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Getter
@@ -30,7 +30,7 @@ public class Comment extends BaseTimeEntity {
 	@Column(nullable = false)
 	private String content;
 
-	/*
+	/* todo : 주석 해제
 	@ManyToOne(fetch = LAZY)
 	@JoinColumn(name = "post_id", nullable = false)
 	private Post post;
@@ -40,10 +40,9 @@ public class Comment extends BaseTimeEntity {
 	@JoinColumn(name = "author_id", nullable = false)
 	private User author;
 
-	public static Comment create(String content, User author) {
-		return Comment.builder()
-			.content(content)
-			.author(author)
+	public static Comment create(String content, User author/*todo: 주석 해제 ,Post post*/) {
+		return Comment.builder().content(content).author(author)
+			//todo: 주석해제 .post(post)
 			.build();
 	}
 
