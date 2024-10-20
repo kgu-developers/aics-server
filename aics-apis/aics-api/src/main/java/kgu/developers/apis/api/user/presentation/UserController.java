b9a7527415f66c7d13a9f0974c0968ac2c305c86
minjo-on <alswns11346@gmail.com>
2024-10-20 20:29:05 +0900

    refactor: 회원가입 에러 swagger 설명 삭제

--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/user/presentation/UserController.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/user/presentation/UserController.java
@@ -5,12 +5,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import kgu.developers.apis.api.user.application.UserService;
 import kgu.developers.apis.api.user.presentation.request.UserCreateRequest;
 import kgu.developers.apis.api.user.presentation.response.UserPersistResponse;
-import kgu.developers.core.common.exception.ExceptionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,18 +23,11 @@ public class UserController {
 	private final UserService userService;
 
 	@Operation(summary = "유저 생성", description = "유저를 생성합니다.")
-	@ApiResponses({
-		@ApiResponse(
-			responseCode = "201",
-			description = "유저 생성 성공",
-			content = @Content(schema = @Schema(implementation = UserPersistResponse.class))
-		),
-		@ApiResponse(
-			responseCode = "409",
-			description = "이미 동일한 학번으로 가입이 되어있습니다.",
-			content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
-		)
-	})
+	@ApiResponse(
+		responseCode = "201",
+		description = "유저 생성 성공",
+		content = @Content(schema = @Schema(implementation = UserPersistResponse.class))
+	)
 	@PostMapping("/signup")
 	public ResponseEntity<UserPersistResponse> signup(
 		@Valid @RequestBody UserCreateRequest request
