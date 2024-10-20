b0dcdf2259cd255670271826002426804255c94f
LeeShinHaeng <dragon11796@gmail.com>
2024-10-21 00:20:30 +0900

    fix: SpringBootApplication 원상복귀

--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/AicsApiApplication.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/AicsApiApplication.java
@@ -2,11 +2,10 @@ package kgu.developers.apis.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class AicsApiApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AicsApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AicsApiApplication.class, args);
+    }
 }
