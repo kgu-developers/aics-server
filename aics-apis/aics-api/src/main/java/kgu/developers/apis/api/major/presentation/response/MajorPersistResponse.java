b634a58638f56464806dbda15e97acab71a99666
LeeShinHaeng <dragon11796@gmail.com>
2024-10-20 22:49:31 +0900

    refactor: response 객체 리턴명 통일

--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/major/presentation/response/MajorPersistResponse.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/major/presentation/response/MajorPersistResponse.java
@@ -1,18 +1,16 @@
 package kgu.developers.apis.api.major.presentation.response;
 
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import kgu.developers.core.domain.major.domain.Major;
 
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
-@JsonNaming()
 public record MajorPersistResponse(
     @Schema(description = "전공식별자", example = "1", requiredMode = REQUIRED)
-    Long majorId,
+    Long id,
 
     @Schema(description = "전공명", example = "AI컴퓨터공학부", requiredMode = REQUIRED)
-    String majorName
+    String name
 ) {
     public static MajorPersistResponse from(Major major) {
         return new MajorPersistResponse(major.getId(), major.getName());
