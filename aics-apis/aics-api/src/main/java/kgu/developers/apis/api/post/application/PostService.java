61c53bf6e192db69788e7aaa3f530773b24ccfcf
minjo-on <alswns11346@gmail.com>
2024-10-24 21:14:28 +0900

    refactor: createPost 메소드 Transaction 어노테이션 추가

--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/post/application/PostService.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/post/application/PostService.java
@@ -2,6 +2,7 @@ package kgu.developers.apis.api.post.application;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import kgu.developers.apis.api.post.presentation.request.PostCreateRequest;
 import kgu.developers.apis.api.post.presentation.response.PostCreateResponse;
 import kgu.developers.core.domain.post.Post;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 public class PostService {
 	private final PostRepository postRepository;
 
+	@Transactional
 	public PostCreateResponse createPost(PostCreateRequest request, User author) {
 		Post savePost = postRepository.save(Post.create(
 			request.title(), request.content(), request.category(), author
