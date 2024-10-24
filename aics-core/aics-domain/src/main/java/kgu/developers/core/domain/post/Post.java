958867bc22f740c46a09530d8f284d5aa7bea1ff
minjo-on <alswns11346@gmail.com>
2024-10-24 18:16:23 +0900

    refactor: post entity content 타입, category nullable 값 변경

--- aics-core/aics-domain/src/main/java/kgu/developers/core/domain/post/Post.java
+++ aics-core/aics-domain/src/main/java/kgu/developers/core/domain/post/Post.java
@@ -1,22 +1,21 @@
 package kgu.developers.core.domain.post;
 
-
-import static jakarta.persistence.CascadeType.ALL;
-import static jakarta.persistence.EnumType.STRING;
-import static lombok.AccessLevel.PROTECTED;
-import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static jakarta.persistence.CascadeType.*;
+import static jakarta.persistence.EnumType.*;
+import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import kgu.developers.core.common.domain.BaseTimeEntity;
 import kgu.developers.core.domain.comment.Comment;
@@ -32,31 +31,32 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 public class Post extends BaseTimeEntity {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "post_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "post_id")
+	private Long id;
 
-    @Column(nullable = false, length = 100)
-    private String title;
+	@Column(nullable = false, length = 100)
+	private String title;
 
-    @Column(nullable = false, length = 3000)
-    private String content;
+	@Column(nullable = false, length = 3000, columnDefinition = "text")
+	private String content;
 
-    @Column(nullable = false)
-    private int views;
+	@Column(nullable = false)
+	private int views;
 
-    @Column(nullable = false)
-    @Enumerated(STRING)
-    private Category category;
+	//TODO: category 확정 후 nullable = true로 변경
+	@Column(nullable = true)
+	@Enumerated(STRING)
+	private Category category;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id")
-    private User author;
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "user_id")
+	private User author;
 
-    @Builder.Default
-    @OneToMany(mappedBy = "post", fetch = LAZY, cascade = ALL, orphanRemoval = true)
-    private List<Comment> comments = new ArrayList<>();
+	@Builder.Default
+	@OneToMany(mappedBy = "post", fetch = LAZY, cascade = ALL, orphanRemoval = true)
+	private List<Comment> comments = new ArrayList<>();
 
     /*
     TODO: 파일 엔티티 생성 후 연결 & create 메서드에 추가
@@ -65,13 +65,13 @@ public class Post extends BaseTimeEntity {
     private File fileID;
     */
 
-    public static Post create(String title, String content, Category category, User author) {
-        return Post.builder()
-            .title(title)
-            .content(content)
-            .views(0)
-            .category(category)
-            .author(author)
-            .build();
-    }
+	public static Post create(String title, String content, Category category, User author) {
+		return Post.builder()
+			.title(title)
+			.content(content)
+			.views(0)
+			.category(category)
+			.author(author)
+			.build();
+	}
 }
