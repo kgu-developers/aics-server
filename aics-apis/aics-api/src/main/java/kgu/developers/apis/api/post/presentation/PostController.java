2f342ff0bd990bc554b892d537613ef650001795
minjo-on <alswns11346@gmail.com>
2024-10-25 09:36:47 +0900

    Revert "refactor: PostController swagger 설명 추가"
    This reverts commit ce0dbd0d9ee35b3041a8682ff0c02b0fa1bef620.

--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/post/presentation/PostController.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/post/presentation/PostController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import kgu.developers.apis.api.post.application.PostService;
 import kgu.developers.apis.api.post.presentation.request.PostCreateRequest;
 import kgu.developers.apis.api.post.presentation.response.PostCreateResponse;
@@ -18,14 +16,9 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
-@Tag(name = "Post", description = "게시글 API")
 public class PostController {
 	private final PostService postService;
 
-	@Operation(summary = "게시글 API", description = """
-			- Description : 이 API는 게시글 생성을 요청합니다.
-			- Assignee : 박민준
-		""")
 	@PostMapping
 	public ResponseEntity<PostCreateResponse> createPost(@RequestBody PostCreateRequest request) {
 		PostCreateResponse response = postService.createPost(request);
--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/post/presentation/request/PostCreateRequest.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/post/presentation/request/PostCreateRequest.java
@@ -5,6 +5,7 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import kgu.developers.core.domain.post.Category;
 
 public record PostCreateRequest(
 	@Schema(description = "게시물 제목", example = "박민준은 살아있다", requiredMode = REQUIRED)
@@ -14,6 +15,12 @@ public record PostCreateRequest(
 
 	@Schema(description = "게시물 내용", example = "살아 숨셔", requiredMode = REQUIRED)
 	@NotBlank(message = "내용은 필수입니다.")
-	String content
+	String content,
+
+	/*
+	@Schema(description = "게시물 카테고리", example = "DEPT_INFO", requiredMode = REQUIRED)
+	@NotNull(message = "카테고리는 필수입니다.")
+	*/
+	Category category
 ) {
 }
