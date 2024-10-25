8d9f71d9721e8ce673d681c4f1d95b8a71e3f6a7
minjo-on <alswns11346@gmail.com>
2024-10-25 09:56:31 +0900

    refactor: PostPersistResponse from 메소드 builder 패턴으로 수정

--- aics-apis/aics-api/src/main/java/kgu/developers/apis/api/post/presentation/response/PostPersistResponse.java
+++ aics-apis/aics-api/src/main/java/kgu/developers/apis/api/post/presentation/response/PostPersistResponse.java
@@ -3,13 +3,16 @@ package kgu.developers.apis.api.post.presentation.response;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.*;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import kgu.developers.core.domain.post.Post;
+import lombok.Builder;
 
+@Builder
 public record PostPersistResponse(
 	@Schema(description = "게시글 id", example = "19991022", requiredMode = REQUIRED)
 	Long postId
 ) {
-	public static PostPersistResponse from(Post post) {
-		return new PostPersistResponse(post.getId());
+	public static PostPersistResponse from(Long postId) {
+		return PostPersistResponse.builder()
+			.postId(postId)
+			.build();
 	}
 }
