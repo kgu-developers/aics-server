3b4328b92766f454c9f59836f96ba5683b092885
minjo-on <alswns11346@gmail.com>
2024-10-28 22:47:17 +0900

    refactor: 게시글 요청 관련 DTO 메세지 삭제

--- aics-api/src/main/java/kgu/developers/api/post/presentation/request/PostUpdateRequest.java
+++ aics-api/src/main/java/kgu/developers/api/post/presentation/request/PostUpdateRequest.java
@@ -8,12 +8,12 @@ import jakarta.validation.constraints.Size;
 
 public record PostUpdateRequest(
 	@Schema(description = "게시물 제목", example = "박민준은 살아있다", requiredMode = REQUIRED)
-	@NotBlank(message = "제목은 필수입니다.")
-	@Size(max = 100, message = "제목은 100자 이내여야 합니다.")
+	@NotBlank
+	@Size(max = 100)
 	String title,
 
 	@Schema(description = "게시물 내용", example = "살아 숨셔", requiredMode = REQUIRED)
-	@NotBlank(message = "내용은 필수입니다.")
+	@NotBlank
 	String content
 ) {
 }
