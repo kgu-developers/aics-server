52c4c2e5b739a544d94747e7e68f96cf04f72b01
minjo-on <alswns11346@gmail.com>
2024-10-31 15:18:00 +0900

    feat: 댓글 조회 로직 구현

--- aics-api/src/main/java/kgu/developers/api/comment/application/CommentService.java
+++ aics-api/src/main/java/kgu/developers/api/comment/application/CommentService.java
@@ -1,14 +1,19 @@
 package kgu.developers.api.comment.application;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import jakarta.transaction.Transactional;
+import kgu.developers.api.comment.presentation.request.CommentListRequest;
 import kgu.developers.api.comment.presentation.request.CommentRequest;
+import kgu.developers.api.comment.presentation.response.CommentListResponse;
 import kgu.developers.api.comment.presentation.response.CommentPersistResponse;
 import kgu.developers.api.post.application.PostService;
 import kgu.developers.api.user.application.UserService;
 import kgu.developers.domain.comment.domain.Comment;
 import kgu.developers.domain.comment.domain.CommentRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +31,11 @@ public class CommentService {
 		);
 		Long id = commentRepository.save(createComment).getId();
 		return CommentPersistResponse.of(id);
+	}
+
+	public CommentListResponse readComments(CommentListRequest request) {
+		List<Comment> comments = commentRepository.findByPostId(request.postId());
+
+		return CommentListResponse.from(comments);
 	}
 }
