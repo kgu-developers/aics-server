a1c2e1725cad35f3db21df6e648dc3a11baca9ed
minjo-on <alswns11346@gmail.com>
2024-10-31 18:04:39 +0900

    refactor: 댓글 관련 dto 어노테이션 수정

--- aics-api/src/main/java/kgu/developers/api/comment/presentation/request/CommentListRequest.java
+++ aics-api/src/main/java/kgu/developers/api/comment/presentation/request/CommentListRequest.java
@@ -3,11 +3,13 @@ package kgu.developers.api.comment.presentation.request;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.*;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record CommentListRequest(
 	@Schema(description = "게시물 아이디", example = "10", requiredMode = REQUIRED)
-	@NotBlank
+	@NotNull
+	@Positive
 	Long postId
 ) {
 }
--- aics-api/src/main/java/kgu/developers/api/comment/presentation/request/CommentRequest.java
+++ aics-api/src/main/java/kgu/developers/api/comment/presentation/request/CommentRequest.java
@@ -1,16 +1,16 @@
 package kgu.developers.api.comment.presentation.request;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.*;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
-
-
-@Builder
 public record CommentRequest(
 	@Schema(description = "게시물 아이디", example = "10", requiredMode = REQUIRED)
-	@NotBlank
+	@NotNull
+	@Positive
 	Long postId,
 
 	@Schema(description = "댓글 내용", example = "예시 코멘트 입니다~~", requiredMode = REQUIRED)
