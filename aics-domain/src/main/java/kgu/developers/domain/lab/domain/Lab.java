680dec5e19215d46d9364a22610c81305cb263a6
minjo-on <alswns11346@gmail.com>
2024-11-01 11:38:10 +0900

    feat: 연구실 엔티티 생성

---
+++ aics-domain/src/main/java/kgu/developers/domain/lab/domain/Lab.java
@@ -0,0 +1,31 @@
+package kgu.developers.domain.lab.domain;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import kgu.developers.common.domain.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class Lab extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 10)
+	private String location;
+
+	@Column(nullable = false, length = 50)
+	private String site;
+}
