c07728cec7f84f34642aaec0aa702e01c34c3c83
minjo-on <alswns11346@gmail.com>
2024-11-01 12:02:53 +0900

    feat: 교수 엔티티 생성

---
+++ aics-domain/src/main/java/kgu/developers/domain/professor/domain/Professor.java
@@ -0,0 +1,40 @@
+package kgu.developers.domain.professor.domain;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import kgu.developers.common.domain.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class Professor extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 10)
+	private String name;
+	
+	@Column(nullable = false, length = 10)
+	private String officeLocation;
+
+	@Column(nullable = false, length = 15)
+	private String contact;
+
+	@Column(nullable = false, length = 50)
+	private String email;
+
+	@Column(nullable = false, length = 15)
+	private String course;
+}
