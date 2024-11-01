1f007c35d439174ae9247a6af07bd639d58c81d6
minjo-on <alswns11346@gmail.com>
2024-11-01 12:13:59 +0900

    feat: 케러셀 엔티티 생성

---
+++ aics-domain/src/main/java/kgu/developers/domain/Carousel/domain/Carousel.java
@@ -0,0 +1,35 @@
+package kgu.developers.domain.Carousel.domain;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import kgu.developers.common.domain.BaseTimeEntity;
+import kgu.developers.domain.file.domain.FileEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class Carousel extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Long order;
+
+	@OneToOne
+	@JoinColumn(name = "file_id", nullable = false)
+	private FileEntity file;
+}
