a02cd24cfead5038630f203bdbc154e7e219f12c
minjo-on <alswns11346@gmail.com>
2024-11-04 21:20:56 +0900

    feat: 연구실 수정 API 구현

--- aics-api/src/main/java/kgu/developers/api/lab/presentation/LabController.java
+++ aics-api/src/main/java/kgu/developers/api/lab/presentation/LabController.java
@@ -4,17 +4,21 @@ import static org.springframework.http.HttpStatus.CREATED;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import kgu.developers.api.lab.application.LabService;
 import kgu.developers.api.lab.presentation.request.LabRequest;
 import kgu.developers.api.lab.presentation.response.LabListResponse;
@@ -51,5 +55,19 @@ public class LabController {
 	public ResponseEntity<LabListResponse> getLabs() {
 		LabListResponse response = labService.getLabs();
 		return ResponseEntity.ok(response);
+	}
+
+	@Operation(summary = "연구실 수정 API", description = """
+			- Description : 이 API는 연구실 정보를 수정합니다.
+			- Assignee : 박민준
+		""")
+	@ApiResponse(responseCode = "204")
+	@PatchMapping("/{id}")
+	public ResponseEntity<Void> updateComment(
+		@Parameter(description = "수정할 연구실 ID", example = "19", required = true) @PathVariable @Positive Long id,
+		@Valid @RequestBody LabRequest request
+	) {
+		labService.updateLab(id, request);
+		return ResponseEntity.noContent().build();
 	}
 }
