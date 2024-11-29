e84162f1f3ac5f949d50f4dcaa187a340daace1c
minjo-on <alswns11346@gmail.com>
2024-11-30 00:27:14 +0900

    refactor: 세미콜론 개행 추가

--- aics-api/src/main/java/kgu/developers/api/about/presentation/Exception/AboutExceptionCode.java
+++ aics-api/src/main/java/kgu/developers/api/about/presentation/Exception/AboutExceptionCode.java
@@ -13,7 +13,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AboutExceptionCode implements ExceptionCode {
 	CATEGORY_NOT_MATCH(BAD_REQUEST, "메인 카테고리와 보조 카테고리가 일치하지 않습니다."),
-	ABOUT_NOT_FOUND(NOT_FOUND, "해당 소개글을 찾을 수 없습니다.");
+	ABOUT_NOT_FOUND(NOT_FOUND, "해당 소개글을 찾을 수 없습니다."),
+	;
 
 	private final HttpStatus status;
 	private final String message;
