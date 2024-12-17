b9d6a6f628dc10ae6cfaf73bca6b023fbc9097e4
LeeShinHaeng <dragon11796@gmail.com>
2024-12-17 22:49:27 +0900

    feat: AccessToken 재발급 로직 구현

--- aics-api/src/main/java/kgu/developers/api/auth/application/AuthService.java
+++ aics-api/src/main/java/kgu/developers/api/auth/application/AuthService.java
@@ -1,21 +1,21 @@
 package kgu.developers.api.auth.application;
 
-import java.time.Duration;
-
-import jakarta.validation.Valid;
+import kgu.developers.api.auth.presentation.exception.TokenNotFoundException;
+import kgu.developers.api.auth.presentation.request.LoginRequest;
 import kgu.developers.api.auth.presentation.request.RefreshTokenRequest;
 import kgu.developers.api.auth.presentation.response.AccessTokenResponse;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import kgu.developers.api.auth.presentation.request.LoginRequest;
 import kgu.developers.api.auth.presentation.response.TokenResponse;
 import kgu.developers.api.user.application.UserService;
 import kgu.developers.common.auth.jwt.TokenProvider;
 import kgu.developers.domain.user.domain.User;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
 
 @Service
 @Builder
@@ -24,6 +24,9 @@ public class AuthService {
 	private final UserService userService;
 	private final PasswordEncoder passwordEncoder;
 	private final TokenProvider tokenProvider;
+	private final RedisTemplate<String, String> redisTemplate;
+
+	private final String REFRESH_TOKEN_KEY_PREFIX = "token:";
 
 	@Transactional(readOnly = true)
 	public TokenResponse login(LoginRequest request) {
@@ -35,13 +38,22 @@ public class AuthService {
 
 		String refreshToken = tokenProvider.generateToken(user.getId(), Duration.ofDays(7));
 		String accessToken = tokenProvider.generateToken(user.getId(), Duration.ofHours(2));
-		// TODO: refresh token 저장
+
+		redisTemplate.opsForValue().set(REFRESH_TOKEN_KEY_PREFIX + refreshToken, userId);
+		redisTemplate.expire(REFRESH_TOKEN_KEY_PREFIX + refreshToken, Duration.ofDays(7));
 
 		return TokenResponse.of(accessToken, refreshToken);
 	}
 
-	// TODO: refresh token 기반 access token 재발급
-	public AccessTokenResponse reissue(@Valid RefreshTokenRequest request) {
-		return null;
+	public AccessTokenResponse reissue(RefreshTokenRequest request) {
+		String requestToken = request.refreshToken();
+		String userId = redisTemplate.opsForValue().get(REFRESH_TOKEN_KEY_PREFIX + requestToken);
+		System.out.println(userId);
+		if (userId == null) {
+			throw new TokenNotFoundException();
+		}
+
+		String accessToken = tokenProvider.generateToken(userId, Duration.ofHours(2));
+		return AccessTokenResponse.of(accessToken);
 	}
 }
--- aics-api/src/main/java/kgu/developers/api/auth/presentation/exception/TokenNotFoundException.java
+++ aics-api/src/main/java/kgu/developers/api/auth/presentation/exception/TokenNotFoundException.java
@@ -3,10 +3,9 @@ package kgu.developers.api.auth.presentation.exception;
 import static kgu.developers.api.auth.presentation.exception.AuthExceptionCode.TOKEN_NOT_FOUND;
 
 import kgu.developers.common.exception.CustomException;
-import kgu.developers.common.exception.ExceptionCode;
 
 public class TokenNotFoundException extends CustomException {
-	public TokenNotFoundException(ExceptionCode code) {
+	public TokenNotFoundException() {
 		super(TOKEN_NOT_FOUND);
 	}
 }
