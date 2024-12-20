3869180c0145dfa1a150e3ba25fcad10074ec754
LeeShinHaeng <dragon11796@gmail.com>
2024-12-20 16:19:05 +0900

    fix: RedisConfig 설정 추가

--- aics-api/src/main/java/kgu/developers/api/AicsApiApplication.java
+++ aics-api/src/main/java/kgu/developers/api/AicsApiApplication.java
@@ -2,12 +2,10 @@ package kgu.developers.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableRedisRepositories(basePackages = "kgu.developers.domain.refreshtoken.domain")
 public class AicsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AicsApiApplication.class, args);
