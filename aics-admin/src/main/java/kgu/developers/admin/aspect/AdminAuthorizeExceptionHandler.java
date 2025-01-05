0396539ade7cd1d41d42678e9006c23f180a2944
LeeShinHaeng <dragon11796@gmail.com>
2025-01-05 19:24:34 +0900

    refactor: 코드 컨벤션 준수를 위한 HttpStatus static import 변경

--- aics-admin/src/main/java/kgu/developers/admin/aspect/AdminAuthorizeExceptionHandler.java
+++ aics-admin/src/main/java/kgu/developers/admin/aspect/AdminAuthorizeExceptionHandler.java
@@ -1,20 +1,19 @@
 package kgu.developers.admin.aspect;
 
 import static kgu.developers.admin.exception.AdminExceptionCode.NOT_ADMIN;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 import kgu.developers.common.exception.ExceptionResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 
 @RestControllerAdvice
 public class AdminAuthorizeExceptionHandler {
 	@ExceptionHandler(AccessDeniedException.class)
 	public ResponseEntity<ExceptionResponse> handleAccessDeniedException() {
 		ExceptionResponse response = ExceptionResponse.from(NOT_ADMIN);
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+		return ResponseEntity.status(FORBIDDEN).body(response);
 	}
 }
