abbc8f49325c257dad6e393965e72559e7b97788
LeeShinHaeng <dragon11796@gmail.com>
2025-01-07 11:13:56 +0900

    refactor: Swagger 예시 데이터 순서 실제 데이터와 통일

--- aics-api/src/main/java/kgu/developers/api/professor/presentation/response/ProfessorListResponse.java
+++ aics-api/src/main/java/kgu/developers/api/professor/presentation/response/ProfessorListResponse.java
@@ -16,8 +16,8 @@ public record ProfessorListResponse(
 			+ "\"id\": 1, "
 			+ "\"name\": \"이은정\", "
 			+ "\"type\": \"조교수\", "
-			+ "\"officeLoc\": \"8213호\", "
 			+ "\"contact\": \"031-249-9671\", "
+			+ "\"officeLoc\": \"8213호\", "
 			+ "\"email\": \"ejlee@kyonggi.ac.kr\", "
 			+ "\"image\": \"https://image.com/professor/profile/image\"}]",
 		requiredMode = REQUIRED)
