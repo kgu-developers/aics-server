d0d301d01f37a0de3e1988d89fcd45d798d361df
minjo-on <alswns11346@gmail.com>
2025-01-13 13:40:43 +0900

    feat: club domain test 작성

--- aics-domain/src/testFixtures/java/club/domain/ClubDomainTest.java
+++ aics-domain/src/testFixtures/java/club/domain/ClubDomainTest.java
@@ -1,4 +1,83 @@
 package club.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import kgu.developers.domain.club.domain.Club;
+
 public class ClubDomainTest {
+	@Test
+	@DisplayName("Club 객체를 생성할 수 있다")
+	public void createClub_Success() {
+		//given
+		String name = "Club A";
+		String description = "a 동아리입니다.";
+		String site = "http://club-a.kyonggi.ac.kr";
+
+		//when
+		Club club = Club.create(name, description, site);
+
+		//then
+		assertNotNull(club);
+		assertEquals(name, club.getName());
+		assertEquals(description, club.getDescription());
+		assertEquals(site, club.getSite());
+	}
+
+	@Test
+	@DisplayName("Club 동아리 이름을 수정할 수 있다")
+	public void updateName_Success() {
+		//given
+		String name = "Club A";
+		String description = "a 동아리입니다.";
+		String site = "http://club-a.kyonggi.ac.kr";
+		Club club = Club.create(name, description, site);
+
+		String newName = "Updated Club A";
+
+		//when
+		club.updateName(newName);
+
+		//then
+		assertEquals(newName, club.getName());
+	}
+
+	@Test
+	@DisplayName("Club 설명을 수정할 수 있다")
+	public void updateDescription_Success() {
+		//given
+		String name = "Club A";
+		String description = "a 동아리입니다.";
+		String site = "http://club-a.kyonggi.ac.kr";
+		Club club = Club.create(name, description, site);
+
+		String newDescription = "club a 동아리입니다.";
+
+		//when
+		club.updateDescription(newDescription);
+
+		//then
+		assertEquals(newDescription, club.getDescription());
+	}
+
+	@Test
+	@DisplayName("Club 사이트를 수정할 수 있다")
+	public void updateSite_Success() {
+		//given
+		String name = "Club A";
+		String description = "a 동아리입니다.";
+		String site = "http://club-a.kyonggi.ac.kr";
+		Club club = Club.create(name, description, site);
+
+		String newSite = "http://Club-a.kyonggi.ac.kr";
+
+		//when
+		club.updateSite(newSite);
+
+		//then
+		assertEquals(newSite, club.getSite());
+	}
 }
