730e338986706318c61001048feb31a0363447f9
minjo-on <alswns11346@gmail.com>
2025-01-16 17:32:51 +0900

    refactor: clubDomainTest 수정

--- aics-domain/src/testFixtures/java/club/domain/ClubDomainTest.java
+++ aics-domain/src/testFixtures/java/club/domain/ClubDomainTest.java
@@ -3,81 +3,72 @@ package club.domain;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import kgu.developers.domain.club.domain.Club;
 
 public class ClubDomainTest {
+	private Club club;
+	private static final String DEFAULT_NAME = "Club A";
+	private static final String DEFAULT_DESCRIPTION = "a 동아리입니다.";
+	private static final String DEFAULT_SITE = "http://club-a.kyonggi.ac.kr";
+
+	@BeforeEach
+	void setUp() {
+		club = Club.create(DEFAULT_NAME, DEFAULT_DESCRIPTION, DEFAULT_SITE);
+	}
+
 	@Test
 	@DisplayName("Club 객체를 생성할 수 있다")
 	public void createClub_Success() {
-		//given
-		String name = "Club A";
-		String description = "a 동아리입니다.";
-		String site = "http://club-a.kyonggi.ac.kr";
 
-		//when
-		Club club = Club.create(name, description, site);
+		// when
+		Club newClub = Club.create(DEFAULT_NAME, DEFAULT_DESCRIPTION, DEFAULT_SITE);
 
-		//then
-		assertNotNull(club);
-		assertEquals(name, club.getName());
-		assertEquals(description, club.getDescription());
-		assertEquals(site, club.getSite());
+		// then
+		assertNotNull(newClub);
+		assertEquals(DEFAULT_NAME, newClub.getName());
+		assertEquals(DEFAULT_DESCRIPTION, newClub.getDescription());
+		assertEquals(DEFAULT_SITE, newClub.getSite());
 	}
 
 	@Test
 	@DisplayName("Club 동아리 이름을 수정할 수 있다")
 	public void updateName_Success() {
-		//given
-		String name = "Club A";
-		String description = "a 동아리입니다.";
-		String site = "http://club-a.kyonggi.ac.kr";
-		Club club = Club.create(name, description, site);
-
+		// given
 		String newName = "Updated Club A";
 
-		//when
+		// when
 		club.updateName(newName);
 
-		//then
+		// then
 		assertEquals(newName, club.getName());
 	}
 
 	@Test
 	@DisplayName("Club 설명을 수정할 수 있다")
 	public void updateDescription_Success() {
-		//given
-		String name = "Club A";
-		String description = "a 동아리입니다.";
-		String site = "http://club-a.kyonggi.ac.kr";
-		Club club = Club.create(name, description, site);
-
 		String newDescription = "club a 동아리입니다.";
 
-		//when
+		// when
 		club.updateDescription(newDescription);
 
-		//then
+		// then
 		assertEquals(newDescription, club.getDescription());
 	}
 
 	@Test
 	@DisplayName("Club 사이트를 수정할 수 있다")
 	public void updateSite_Success() {
-		//given
-		String name = "Club A";
-		String description = "a 동아리입니다.";
-		String site = "http://club-a.kyonggi.ac.kr";
-		Club club = Club.create(name, description, site);
-
+		// given
 		String newSite = "http://Club-a.kyonggi.ac.kr";
 
-		//when
+		// when
 		club.updateSite(newSite);
 
-		//then
+		// then
 		assertEquals(newSite, club.getSite());
 	}
 }
