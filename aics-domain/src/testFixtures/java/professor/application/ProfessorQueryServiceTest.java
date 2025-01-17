799b0fba171b83172ea0e8b18dce0fc2721bb03a
minjo-on <alswns11346@gmail.com>
2025-01-17 14:08:59 +0900

    refactor: professorQueryServiceTest 추가 작성

--- aics-domain/src/testFixtures/java/professor/application/ProfessorQueryServiceTest.java
+++ aics-domain/src/testFixtures/java/professor/application/ProfessorQueryServiceTest.java
@@ -4,6 +4,7 @@ import static kgu.developers.domain.professor.domain.Role.ASSISTANT;
 import static kgu.developers.domain.professor.domain.Role.PROFESSOR;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
@@ -53,8 +54,28 @@ public class ProfessorQueryServiceTest {
 	}
 
 	@Test
-	@DisplayName("getProfessor는 존재하지 않는 교수를 찾아올 경우 ProfessorNotFoundException을 발생시킨다.")
-	public void getProfessor_NotFound_ThrowsException() {
+	@DisplayName("getProfessorById는 해당 교수를 조회할 수 있다")
+	public void getProfessorById_Success() {
+		// given
+		Long professorId = 1L;
+
+		// when
+		Professor result = professorQueryService.getProfessorById(professorId);
+
+		// then
+		assertNotNull(result);
+		assertEquals(professorId, result.getId());
+		assertEquals("alswns11346@kyonggi.ac.kr", result.getEmail());
+		assertEquals("박민준", result.getName());
+		assertEquals(ASSISTANT, result.getRole());
+		assertEquals("010-1234-5678", result.getContact());
+		assertEquals("img1", result.getImg());
+		assertEquals("office1", result.getOfficeLoc());
+	}
+
+	@Test
+	@DisplayName("getProfessorById는 존재하지 않는 교수를 찾아올 경우 ProfessorNotFoundException을 발생시킨다.")
+	public void getProfessorById_NotFound_ThrowsException() {
 		// given
 		Long professorId = 4L;
 
