81ff8e6a34cd7a2a3b860b4c511765287bf3a91c
LeeShinHeang <dragon11796@gmail.com>
2025-02-20 22:41:07 +0900

    refactor: 스웨거 예시 데이터 변경

--- aics-admin/src/main/java/kgu/developers/admin/user/presentation/request/UserKickOutListRequest.java
+++ aics-admin/src/main/java/kgu/developers/admin/user/presentation/request/UserKickOutListRequest.java
@@ -11,7 +11,7 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @Builder
 public record UserKickOutListRequest(
-	@Schema(description = "학번 리스트", example = "[\"202412345\", \"202412346\"]", requiredMode = REQUIRED)
+	@Schema(description = "학번 리스트", example = "[\"202412347\", \"202412349\"]", requiredMode = REQUIRED)
 	@NotEmpty(message = "학번 리스트는 비어 있을 수 없습니다.")
 	List<@Pattern(regexp = "\\d{9}", message = "학번은 9자리 숫자로 입력해야 합니다.") String> userIds
 ) {
