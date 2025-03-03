dc8421663bb2b7b0b7b197d54265b9a042270a9f
LeeShinHeang <dragon11796@gmail.com>
2025-03-03 22:18:21 +0900

    fix: data.sql 시간 변경

--- aics-api/src/main/resources/db/data.sql
+++ aics-api/src/main/resources/db/data.sql
@@ -1,15 +1,14 @@
 -- about
 INSERT INTO about (content, category, created_at, updated_at)
 VALUES ('학과 소개 내용입니다.', 'DEPT_INTRO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('경기대학교 수원캠퍼스 8강의동에 있습니다.', 'DIRECTIONS', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('이런 저런 동아리가 있습니다.', 'CLUB', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('경기대학교 수원캠퍼스 8강의동에 있습니다.', 'DIRECTIONS', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- club
 INSERT INTO club (name, description, site, created_at, updated_at)
-VALUES ('SSF', '웹 개발 동아리 SSF입니다.', 'https://www.ssf.or.kr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('K.Knock', '경기대학교 정보보안 동아리 K.Knock입니다.', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('C-Lab', '경기대학교 AI컴퓨터공학부 개발동아리입니다.', 'https://clab.page', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('InQ', '개발자 플랫폼 동아리 InQ입니다.', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES ('SSF', '웹 개발 동아리 SSF입니다.', 'https://www.ssf.or.kr', '2025-03-03 00:00:00', '2025-03-03 00:00:00'),
+       ('K.Knock', '경기대학교 정보보안 동아리 K.Knock입니다.', NULL, '2025-03-03 01:00:00', '2025-03-03 01:00:00'),
+       ('C-Lab', '경기대학교 AI컴퓨터공학부 개발동아리입니다.', 'https://clab.page', '2025-03-03 02:00:00', '2025-03-03 02:00:00'),
+       ('InQ', '개발자 플랫폼 동아리 InQ입니다.', NULL, '2025-03-03 03:00:00', '2025-03-03 03:00:00');
 
 -- password: password1234!
 INSERT INTO "user" (id, password, name, email, phone, major, role, created_at, updated_at)
@@ -27,15 +26,15 @@ VALUES ('202412346', '$2a$10$NkQj6yk0Xh4QhKevjrOkouQBymXUgpKqmHQFnTUKRaVhDrRZf5O
 -- professor
 INSERT INTO professor (name, contact, email, img, office_loc ,role, created_at, updated_at)
 VALUES ('김교수', '010-1111-2222', 'kim@kyonggi.ac.kr', 'https://image.com/professor/profile/image1',
-        '8501호','PROFESSOR', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+        '8501호','PROFESSOR', '2025-03-03 00:00:00', '2025-03-03 00:00:00'),
        ('이조교', '010-3333-4444', 'lee@kgu.ac.kr', 'https://image.com/professor/profile/image2',
-        '제2공학관 601호' ,'ASSISTANT', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+        '제2공학관 601호' ,'ASSISTANT', '2025-03-03 01:00:00', '2025-03-03 01:00:00'),
        ('박교수', '010-5555-6666', 'park@kyonggi.ac.kr', 'https://image.com/professor/profile/image3',
-        '제2공학관 601호','PROFESSOR', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+        '제2공학관 601호','PROFESSOR', '2025-03-03 02:00:00', '2025-03-03 02:00:00'),
        ('최교수', '010-7777-8888', 'choi@kyonggi.ac.kr', 'https://image.com/professor/profile/none',
-        '8201호','PROFESSOR', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+        '8201호','PROFESSOR', '2025-03-03 03:00:00', '2025-03-03 03:00:00'),
        ('강교수', '010-9999-0000', 'kang@kyonggi.ac.kr', 'https://image.com/professor/profile/none',
-        '8201호','PROFESSOR', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+        '8201호','PROFESSOR', '2025-03-03 04:00:00', '2025-03-03 04:00:00');
 
 -- fileEntity
 INSERT INTO file_entity (logical_name, physical_path, extension, file_size, created_at, updated_at)
@@ -87,44 +86,44 @@ VALUES (4, '최신 연구 성과를 확인하세요.', 'https://kyonggi.ac.kr/re
 INSERT INTO post (title, content, category, file_id, author_id, created_at, updated_at, views, is_pinned)
 VALUES ('2024학년도 학과 소개 일정 안내',
         '2024학년도 학과 소개가 아래와 같은 일정으로 진행됩니다. 참여를 원하시는 분들은 해당 일정을 참고하여 신청해 주시기 바랍니다.',
-        'NOTIFICATION', 1, '202412346', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, false),
+        'NOTIFICATION', 1, '202412346', '2025-03-03 00:00:00', '2025-03-03 00:00:00', 0, false),
        ('동계 방학 중 실습실 사용 안내',
         '동계 방학 기간 동안 실습실 사용 신청을 받고 있습니다. 자세한 신청 방법과 이용 규정을 확인해 주세요.',
-        'NOTIFICATION', NULL, '202412346', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, false),
+        'NOTIFICATION', NULL, '202412346', '2025-03-03 01:00:00', '2025-03-03 01:00:00', 0, false),
        ('취업 설명회 개최 안내',
         '졸업 예정자 및 재학생을 대상으로 취업 설명회를 개최합니다. 기업 소개, 취업 전략 및 질의응답 시간이 준비되어 있으니 많은 참여 바랍니다.',
-        'NOTIFICATION', 9, '202412346', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, false),
+        'NOTIFICATION', 9, '202412346', '2025-03-03 02:00:00', '2025-03-03 02:00:00', 0, false),
        ('학과 뉴스 - 최신 연구 성과 발표',
         '우리 학과에서는 최근 AI 기반 의료 데이터 분석 연구 성과를 발표하였습니다. 자세한 내용은 연구 성과 페이지를 참고하세요.',
-        'NEWS', 4, '202412347', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, false),
+        'NEWS', 4, '202412347', '2025-03-03 03:00:00', '2025-03-03 03:00:00', 0, false),
        ('졸업생 수상 소식',
         '우리 학과 졸업생이 국내 최고 권위의 학회에서 우수 논문상을 수상하였습니다. 졸업생들의 뛰어난 활약을 소개합니다.',
-        'NEWS', 8, '202412347', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, false),
+        'NEWS', 8, '202412347', '2025-03-03 04:00:00', '2025-03-03 04:00:00', 0, false),
        ('동아리 활동 현황',
         '동아리 활동 사진과 관련 자료를 확인하세요. 동아리 가입 및 활동 절차에 대해 설명합니다.',
-        'NOTIFICATION', 6, '202412348', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, false),
+        'NOTIFICATION', 6, '202412348', '2025-03-03 05:00:00', '2025-03-03 05:00:00', 0, false),
        ('캠퍼스 전경과 주요 시설 안내',
         '캠퍼스 전경 및 주요 시설에 대한 소개입니다. 사진과 함께 확인해보세요.',
-        'NOTIFICATION', 7, '202412348', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, false),
+        'NOTIFICATION', 7, '202412348', '2025-03-03 06:00:00', '2025-03-03 06:00:00', 0, false),
        ('연구 프로젝트 발표',
         'AI 기반 연구 프로젝트에 대한 발표 자료입니다. 관심 있는 분들은 파일을 다운로드하세요.',
-        'NEWS', 10, '202412346', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, false);
+        'NEWS', 10, '202412346', '2025-03-03 07:00:00', '2025-03-03 07:00:00', 0, false);
 
 -- comment
 INSERT INTO comment (content, created_at, updated_at, post_id, author_id)
-VALUES ('참여 신청 방법을 자세히 알고 싶습니다.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, '202412347'),
-       ('일정 확인 감사합니다!', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1, '202412348'),
-       ('실습실 신청은 어디서 할 수 있나요?', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2, '202412347'),
-       ('규정 관련 자료를 추가로 공유 부탁드립니다.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2, '202412346'),
-       ('설명회 시간은 몇 시부터인가요?', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3, '202412348'),
-       ('참여 신청 링크는 어디인가요?', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3, '202412347'),
-       ('AI 연구 성과에 대한 자세한 자료를 보고 싶습니다.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 4, '202412346'),
-       ('다음 연구 발표 일정이 궁금합니다.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 4, '202412348'),
-       ('졸업생들 대단합니다! 축하드립니다.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 5, '202412347'),
-       ('우수 논문상 수상 자료를 확인할 수 있을까요?', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 5, '202412346'),
-       ('동아리 가입 방법을 알고 싶습니다.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 6, '202412348'),
-       ('활동 사진을 보니 흥미롭네요.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 6, '202412347'),
-       ('캠퍼스 전경이 정말 아름답네요.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 7, '202412346'),
-       ('주요 시설 이용 안내도 포함되면 좋겠습니다.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 7, '202412348'),
-       ('프로젝트 자료를 다운로드했습니다. 유익하네요.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 8, '202412347'),
-       ('연구 참여 방법이 있나요?', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 8, '202412346');
+VALUES ('참여 신청 방법을 자세히 알고 싶습니다.', '2025-03-03 00:01:00', '2025-03-03 00:01:00', 1, '202412347'),
+       ('일정 확인 감사합니다!', '2025-03-03 00:02:00', '2025-03-03 00:02:00', 1, '202412348'),
+       ('실습실 신청은 어디서 할 수 있나요?', '2025-03-03 01:01:00', '2025-03-03 01:01:00', 2, '202412347'),
+       ('규정 관련 자료를 추가로 공유 부탁드립니다.', '2025-03-03 01:02:00', '2025-03-03 01:02:00', 2, '202412346'),
+       ('설명회 시간은 몇 시부터인가요?', '2025-03-03 02:01:00', '2025-03-03 02:01:00', 3, '202412348'),
+       ('참여 신청 링크는 어디인가요?', '2025-03-03 02:02:00', '2025-03-03 02:02:00', 3, '202412347'),
+       ('AI 연구 성과에 대한 자세한 자료를 보고 싶습니다.', '2025-03-03 03:01:00', '2025-03-03 03:01:00', 4, '202412346'),
+       ('다음 연구 발표 일정이 궁금합니다.', '2025-03-03 03:02:00', '2025-03-03 03:02:00', 4, '202412348'),
+       ('졸업생들 대단합니다! 축하드립니다.', '2025-03-03 04:01:00', '2025-03-04 04:01:00', 5, '202412347'),
+       ('우수 논문상 수상 자료를 확인할 수 있을까요?', '2025-03-03 04:02:00', '2025-03-03 04:02:00', 5, '202412346'),
+       ('동아리 가입 방법을 알고 싶습니다.', '2025-03-03 05:01:00', '2025-03-03 05:01:00', 6, '202412348'),
+       ('활동 사진을 보니 흥미롭네요.', '2025-03-03 05:02:00', '2025-03-03 05:02:00', 6, '202412347'),
+       ('캠퍼스 전경이 정말 아름답네요.', '2025-03-03 06:01:00', '2025-03-03 06:01:00', 7, '202412346'),
+       ('주요 시설 이용 안내도 포함되면 좋겠습니다.', '2025-03-03 06:02:00', '2025-03-03 06:02:00', 7, '202412348'),
+       ('프로젝트 자료를 다운로드했습니다. 유익하네요.', '2025-03-03 07:01:00', '2025-03-03 07:01:00', 8, '202412347'),
+       ('연구 참여 방법이 있나요?', '2025-03-03 07:02:00', '2025-03-03 07:02:00', 8, '202412346');
