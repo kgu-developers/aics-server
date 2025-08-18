bed0a6a50e54c3553d5290488487ef5e6134c9a5
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-08-18 18:10:37 +0900

    chore: 애플리케이션에 태그 추가

--- deploy/staging/deploy.sh
+++ deploy/staging/deploy.sh
@@ -5,8 +5,11 @@ APP_HOME=$(pwd)/backend
 
 echo "===== Loading Docker images ====="
 sudo docker load -i $APP_HOME/tar/aics-api.tar
+sudo docker tag aics-admin:${GITHUB_SHA} aics-admin:latest
 sudo docker load -i $APP_HOME/tar/aics-admin.tar
+sudo docker tag aics-auth:${GITHUB_SHA} aics-auth:latest
 sudo docker load -i $APP_HOME/tar/aics-auth.tar
+sudo docker tag aics-api:${GITHUB_SHA} aics-api:latest
 
 echo "===== Stopping existing containers ====="
 sudo docker compose -f $APP_HOME/docker-compose.yml down || true
