c1fe2699e3a0104a77b00e517a9ac1e6a498745f
dkdltm221 <dkdltm221@naver.com>
2025-08-31 17:55:13 +0900

    fix/#252 fileId 조회 실패 null반환

--- aics-domain/src/main/java/kgu/developers/domain/file/application/query/FileQueryService.java
+++ aics-domain/src/main/java/kgu/developers/domain/file/application/query/FileQueryService.java
@@ -2,7 +2,6 @@ package kgu.developers.domain.file.application.query;
 
 import kgu.developers.domain.file.domain.FileEntity;
 import kgu.developers.domain.file.domain.FileRepository;
-import kgu.developers.domain.file.exception.FileNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +13,7 @@ public class FileQueryService {
 	private final FileRepository fileRepository;
 
 	public FileEntity getFileById(Long id) {
-		return fileRepository.findById(id)
-				.orElseThrow(FileNotFoundException::new);
+		return fileRepository.findById(id).orElse(null);
 	}
 
 	public List<FileEntity> findAllByIds(List<Long> ids) {
