436c5bf61b47a3988536395d446c5bd4aa71a9e3
JangYeongHu <jyhoo040912@gmail.com>
2025-09-02 22:11:57 +0900

    test: 테스트용 작성자ID, 게시글 ID 상수화

--- aics-domain/src/testFixtures/java/comment/application/CommentCommandServiceTest.java
+++ aics-domain/src/testFixtures/java/comment/application/CommentCommandServiceTest.java
@@ -55,7 +55,7 @@ public class CommentCommandServiceTest {
 		);
 		fakePostRepository.save(Post.create(
 			"SW 부트캠프 4기 교육생 모집", "SW전문인재양성사업단에서는 SW부트캠프 4기 교육생을 모집합니다.", NEWS,
-			"202412345", null, false
+			TEST_USER_ID, null, false
 		));
 	}
 
@@ -95,8 +95,8 @@ public class CommentCommandServiceTest {
 	@DisplayName("updateComment는 댓글을 수정할 수 있다.")
 	public void updateComment_Success() {
 		// given
-		Comment comment = Comment.create("SW 부트캠프 모집이 정말 기대됩니다.", "202312345",
-			1L);
+		Comment comment = Comment.create("SW 부트캠프 모집이 정말 기대됩니다.", TEST_USER_ID,
+			TEST_POST_ID);
 		String updatedCommentContent = "SW 부트캠프 모집이 정말 기대됩니다. 4기 교육생 확정이 언제일까요?";
 
 		// when
@@ -110,8 +110,8 @@ public class CommentCommandServiceTest {
 	@DisplayName("deleteComment는 댓글을 삭제할 수 있다.")
 	public void deleteComment_Success() {
 		// given
-		Comment comment = Comment.create("SW 부트캠프 모집이 정말 기대됩니다.", "202312345",
-			1L);
+		Comment comment = Comment.create("SW 부트캠프 모집이 정말 기대됩니다.", TEST_USER_ID,
+			TEST_POST_ID);
 		assertNull(comment.getDeletedAt(), "삭제 전에는 deletedAt이 null이어야 합니다");
 
 		// when
