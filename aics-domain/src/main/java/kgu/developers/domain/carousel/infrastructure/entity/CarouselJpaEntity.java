748e98ebb9b30cdf87b1878d57592d906a183430
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-09-20 17:38:48 +0900

    refactor: 테이블 이름 명시

--- aics-domain/src/main/java/kgu/developers/domain/carousel/infrastructure/entity/CarouselJpaEntity.java
+++ aics-domain/src/main/java/kgu/developers/domain/carousel/infrastructure/entity/CarouselJpaEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import kgu.developers.common.domain.BaseTimeEntity;
 import kgu.developers.domain.carousel.domain.Carousel;
 import lombok.AllArgsConstructor;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Builder
+@Table(name = "carousel")
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 public class CarouselJpaEntity extends BaseTimeEntity {
--- aics-domain/src/main/java/kgu/developers/domain/file/infrastructure/entity/FileJpaEntity.java
+++ aics-domain/src/main/java/kgu/developers/domain/file/infrastructure/entity/FileJpaEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import kgu.developers.common.domain.BaseTimeEntity;
 import kgu.developers.domain.file.domain.FileModel;
 import lombok.AllArgsConstructor;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Builder
+@Table(name = "file_entity")
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 public class FileJpaEntity extends BaseTimeEntity {
