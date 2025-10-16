584b3c6547826f0cd29e69c4b2d587a2610be98c
Jooyunbin <dkdltm221@naver.com>
2025-10-16 13:06:30 +0900

    fix/club-entity-revert: 기존 Club 도메인 복원

--- aics-domain/src/main/java/kgu/developers/domain/club/domain/Club.java
+++ aics-domain/src/main/java/kgu/developers/domain/club/domain/Club.java
@@ -1,38 +1,20 @@
 package kgu.developers.domain.club.domain;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-import static lombok.AccessLevel.PROTECTED;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import kgu.developers.common.domain.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Entity
+
+
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor(access = PROTECTED)
-public class Club extends BaseTimeEntity {
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
+public class Club{
+
 	private Long id;
-
-	@Column(nullable = false, unique = true, length = 16)
 	private String name;
-
-	@Column(nullable = false, length = 100)
 	private String description;
-
-	@Column(length = 50)
 	private String site;
-
-	@Column(name = "file_id")
 	private Long fileId;
 
 	public static Club create(String name, String description, String site, Long fileId) {
