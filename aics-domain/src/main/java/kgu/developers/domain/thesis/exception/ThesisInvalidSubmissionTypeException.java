8735dae251bfedb06d2832f9f7b72c0e9a310df5
JangYeongHu <jyhoo040912@gmail.com>
2026-01-15 21:05:45 +0900

    fix: 잘못된 에러코드값 수정

--- aics-domain/src/main/java/kgu/developers/domain/thesis/exception/ThesisInvalidSubmissionTypeException.java
+++ aics-domain/src/main/java/kgu/developers/domain/thesis/exception/ThesisInvalidSubmissionTypeException.java
@@ -2,10 +2,10 @@ package kgu.developers.domain.thesis.exception;
 
 import kgu.developers.common.exception.CustomException;
 
-import static kgu.developers.domain.thesis.exception.ThesisDomainExceptionCode.THESIS_INVALID_GRADUATION_TYPE_EXCEPTION;
+import static kgu.developers.domain.thesis.exception.ThesisDomainExceptionCode.THESIS_INVALID_SUBMISSION_TYPE_EXCEPTION;
 
 public class ThesisInvalidSubmissionTypeException extends CustomException {
     public ThesisInvalidSubmissionTypeException() {
-        super(THESIS_INVALID_GRADUATION_TYPE_EXCEPTION);
+        super(THESIS_INVALID_SUBMISSION_TYPE_EXCEPTION);
     }
 }
