d0c9a844d4f9b7c44d1b949703b2fc319c2cfd80
JangYeongHu <jyhoo040912@gmail.com>
2026-01-15 21:15:53 +0900

    refactor: ThesisSubmitRequest Schema 추가

--- aics-api/src/main/java/kgu/developers/api/thesis/presentation/request/ThesisSubmitRequest.java
+++ aics-api/src/main/java/kgu/developers/api/thesis/presentation/request/ThesisSubmitRequest.java
@@ -1,9 +1,13 @@
 package kgu.developers.api.thesis.presentation.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import kgu.developers.domain.schedule.domain.SubmissionType;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 public record ThesisSubmitRequest(
+	@Schema(description = "졸업 논문 유형", example = "MIDTHESIS", requiredMode = REQUIRED)
 	@NotNull(message = "졸업 논문 유형은 필수입니다.")
 	SubmissionType type
 ) {
